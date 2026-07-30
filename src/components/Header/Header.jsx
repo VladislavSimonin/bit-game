27653d74b26d23d1cfdda9ba826e7b7fd4a2ce3d
VladislavSimonin <vladislavsimonin@gmail.com>
2026-07-30 14:54:16 +0300

    Создание <header>

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,14 @@
+import '../Header/Header.css'
 
 export default function Header() {
-    
+    return (
+        <header>
+            <img className='img-logo' src='/public/android-chrome-192x192.png' />
+            
+            <div className='nav-icons'>
+                <img className='img-icons' src='/public/search.png' />
+                <img className='img-icons' src='/public/align-justify_25181.png' />
+            </div>
+        </header>
+    )
 }
