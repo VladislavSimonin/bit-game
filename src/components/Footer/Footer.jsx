b30f82f5d62bd1de27c969f66c0e898f5dd1f49c
VladislavSimonin <vladislavsimonin@gmail.com>
2026-09-19 14:59:37 +0300

    процесс создания ссылок на соцсети

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,18 @@
 
 export default function Footer() {
-    return(
+    return (
         <footer>
-            <a href='https://github.com/VladislavSimonin'>
-                <img src='../public/github.png'></img>
-            </a>
+            <div className='social'>
+                <a href='https://github.com/VladislavSimonin'>
+                    <img src='../public/github.png'></img>
+                </a>
+                <a href='https://t.me/vlad_fq14085'>
+                    <img src='../public/telegram.png'></img>
+                </a>
+                <a href='mailto:vladislavforworkmail@gmail.com'>
+                    <img src='../public/gmail.png'></img>
+                </a>
+            </div>
         </footer>
     )
 }
