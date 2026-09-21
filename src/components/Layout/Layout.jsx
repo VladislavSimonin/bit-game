57474a4dfa1edc28a511ee340dd589c69467c480
VladislavSimonin <vladislavsimonin@gmail.com>
2026-09-21 19:04:47 +0300

    исправление ошибок с flex

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,12 +1,13 @@
 import { Outlet } from 'react-router-dom'
 import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
+import '../Layout/Layout.css'
 
 export default function Layout() {
     return(
         <>
             <Header />
-            <main>
+            <main className='main-page'>
                 <Outlet /> {/* Сюда подставится текущая страница */}
             </main>
             <Footer />
