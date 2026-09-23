0b6b7751598c7619ecef284edde4a3c35584c590
VladislavSimonin <vladislavsimonin@gmail.com>
2026-09-23 15:58:07 +0300

    сделан первоначальный footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,20 +4,20 @@ export default function Footer() {
     return (
         <footer className='site-footer'>
             <div className='footer-content'>
-                <div className='footer-section-nav'>
-                    <h3>О проекте</h3>
-                </div>
                 <div className='social'>
-                    <h3>Контакты</h3>
                     <a className='social-img' href='https://github.com/VladislavSimonin' target='_blank' rel='noopener noreferrer'>
-                        <img src={`${import.meta.env.BASE_URL}github.png`} alt="GitHub"></img>
+                        <img className='social-icons' src={`${import.meta.env.BASE_URL}github.png`} alt="GitHub"></img>
                     </a>
                     <a className='social-img' href='https://t.me/vlad_fq14085' target='_blank' rel='noopener noreferrer'>
-                        <img src={`${import.meta.env.BASE_URL}telegram.png`} alt="Telegram"></img>
+                        <img className='social-icons' src={`${import.meta.env.BASE_URL}telegram.png`} alt="Telegram"></img>
                     </a>
                     <a className='social-img' href='mailto:vladislavforworkmail@gmail.com' target='_blank' rel='noopener noreferrer'>
-                        <img src={`${import.meta.env.BASE_URL}gmail.png`} alt="Gmail"></img>
+                        <img className='social-icons' src={`${import.meta.env.BASE_URL}gmail.png`} alt="Gmail"></img>
                     </a>
+                </div>
+
+                <div className='footer-section-bottom'>
+                     <p className='copyright'>&copy; 2026 Bit Game. Все права защищены.</p>
                 </div>
             </div>
 
